517b8fb9eb66d1bb0a6efad34728122af4a5b42c
Valentin <valentin.reichenecker@hs-furtwangen.de>
2019-05-16 11:42:09 +0200

    reidentifikation in funktion ausgelagert um code besser lesbar zu machen

--- src/Gesichtsreidentifikation.py
+++ src/Gesichtsreidentifikation.py
@@ -28,6 +28,8 @@ input_blob = []
 out_blob = []
 exec_net = []
 exec_net_reid = []
+cap = None
+
 
 def buildargparser():
     parser = ArgumentParser()
@@ -54,14 +56,16 @@ def personGallery(face_gallery):
                 break
 
             image = cv2.imread(path)
-            reId = reidentification(image)
+            _, _, _, _, reId = reidentification(image)
             globalReIdVec.append(reId)
             names[id] = label
             id += 1
 
 
 def initReidentification():
-    global exec_net, exec_net_reid, input_blob, out_blob, model_n, model_c, model_h, model_w,input_blob_reid,out_blob_reid,model_reid_n, model_reid_c, model_reid_h, model_reid_w
+    global exec_net, exec_net_reid, input_blob, out_blob, model_n, model_c, model_h, model_w, input_blob_reid, out_blob_reid, model_reid_n, model_reid_c, model_reid_h, model_reid_w
+    global cap
+    cap = cv2.VideoCapture(0)
     net = IENetwork(model=model_xml, weights=model_bin)
     net_reid = IENetwork(model=model_reid_xml, weights=model_reid_bin)
 
@@ -71,7 +75,7 @@ def initReidentification():
     plugin_reid = IEPlugin(device="CPU")
     plugin_reid.add_cpu_extension(
         "/home/reichenecker/inference_engine_samples_build/intel64/Release/lib/libcpu_extension.so")
-    plugin_reid.set_config(net_reid)
+    # plugin_reid.set_config(net_reid)
     exec_net = plugin.load(network=net, num_requests=1)
     exec_net_reid = plugin_reid.load(network=net_reid, num_requests=1)
 
@@ -91,8 +95,10 @@ def initReidentification():
 
 
 def reidentification(image):
+    global exec_net
 
-
+    cap_w = cap.get(3)
+    cap_h = cap.get(4)
     im = cv2.resize(image, (640, 480))
 
     in_frame = cv2.resize(im, (model_w, model_h))
@@ -107,10 +113,10 @@ def reidentification(image):
             class_id = int(obj[1])
             if class_id == 1:
                 if obj[2] > 0.5:
-                    xmin = int(obj[3] * 640)
-                    ymin = int(obj[4] * 480)
-                    xmax = int(obj[5] * 640)
-                    ymax = int(obj[6] * 480)
+                    xmin = int(obj[3] * cap_w)
+                    ymin = int(obj[4] * cap_h)
+                    xmax = int(obj[5] * cap_w)
+                    ymax = int(obj[6] * cap_h)
                     frame_org = im.copy()
                     person = frame_org[ymin:ymax, xmin:xmax]
                     in_frame_reid = cv2.resize(person, (model_reid_w, model_reid_h))
@@ -123,8 +129,8 @@ def reidentification(image):
                         res_reid = exec_net_reid.requests[0].outputs[out_blob_reid]
                         reIdVector = res_reid[0].reshape(-1, )
 
-                        return reIdVector
-
+                        return xmin, ymin, xmax, ymax, reIdVector
+    return 0,0,0,0, None
 
 def createMatchingPerson(newReIdVec):
     global globalReIdVec
@@ -164,13 +170,13 @@ def cosineSimilarity(u,
 
 
 def main():
-    global exec_net, exec_net_reid
+    global exec_net, exec_net_reid, cap
     args = buildargparser().parse_args();
     initReidentification()
     personGallery(args.person_gallery)
-    #loadAllowedFaces()
+    # loadAllowedFaces()
 
-    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture("http://192.168.0.35/cgi-bin/videostream.cgi?user=admin&pwd=admin")
     print("init done")
 
     while True:
@@ -178,50 +184,22 @@ def main():
         if not ret:
             break
 
-        cap_w = cap.get(3)
-        cap_h = cap.get(4)
-        in_frame = cv2.resize(frame, (model_w, model_h))
-        in_frame = in_frame.transpose((2, 0, 1))
-        in_frame = in_frame.reshape((model_n, model_c, model_h, model_w))
+        xmin, ymin, xmax, ymax, reIdVector = reidentification(frame)
+        if reIdVector is not None:
+            foundId = findMatchingPerson(reIdVector)
+        else:
+            foundId = -1
 
-        exec_net.start_async(request_id=0, inputs={input_blob: in_frame})
+        color = (255, 0, 0)
+        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
+        idColor = (0, 0, 255)
+        print(foundId)
+        if foundId is not -1:
+            cv2.putText(frame, names[foundId], (xmin, ymin - 7), cv2.FONT_HERSHEY_COMPLEX, 0.8, idColor,
+                        1)
+        else:
+            cv2.putText(frame, "Unknown", (xmin, ymin - 7), cv2.FONT_HERSHEY_COMPLEX, 0.8, idColor, 1)
 
-        if exec_net.requests[0].wait(-1) == 0:
-            res = exec_net.requests[0].outputs[out_blob]
-
-            for obj in res[0][0]:
-                class_id = int(obj[1])
-                if class_id == 1:
-                    if obj[2] > 0.5:
-                        xmin = int(obj[3] * cap_w)
-                        ymin = int(obj[4] * cap_h)
-                        xmax = int(obj[5] * cap_w)
-                        ymax = int(obj[6] * cap_h)
-
-                        frame_org = frame.copy()
-                        person = frame_org[ymin:ymax, xmin:xmax]
-                        in_frame_reid = cv2.resize(person, (model_reid_w, model_reid_h))
-                        in_frame_reid = in_frame_reid.transpose((2, 0, 1))
-                        in_frame_reid = in_frame_reid.reshape((model_reid_n, model_reid_c, model_reid_h, model_reid_w))
-                        cv2.imshow("eksde", person)
-
-                        exec_net_reid.start_async(request_id=0, inputs={input_blob_reid: in_frame_reid})
-                        foundId = 1;
-                        if exec_net_reid.requests[0].wait(-1) == 0:
-                            res_reid = exec_net_reid.requests[0].outputs[out_blob_reid]
-                            reIdVector = res_reid[0].reshape(-1, )
-                            # print(reIdVector)
-                            foundId = findMatchingPerson(reIdVector)
-
-                        color = (255, 0, 0)
-                        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
-                        idColor = (0, 0, 255)
-                        print(foundId)
-                        if foundId is not -1:
-                            cv2.putText(frame, names[foundId], (xmin, ymin - 7), cv2.FONT_HERSHEY_COMPLEX, 0.8, idColor,
-                                        1)
-                        else:
-                            cv2.putText(frame, "Unknown", (xmin, ymin - 7), cv2.FONT_HERSHEY_COMPLEX, 0.8, idColor, 1)
 
         cv2.imshow("Facerecognition", frame)
 
@@ -232,7 +210,6 @@ def main():
     cv2.destroyAllWindows()
     del exec_net
     del exec_net_reid
-
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
